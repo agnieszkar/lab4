b966d8bce4eec93262ee8f22fccf85d623aa0e29
agnieszkar <agnieszkar2012@gmail.com>
2015-11-28 17:51:12 +0100

    Replaced for with  Lambda expression

--- src/com/MainZad2.java
+++ src/com/MainZad2.java
@@ -1,14 +1,13 @@
 package com;
 
-import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 
 public class MainZad2 {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ParallelPrimeGenerator parallelPrimeGenerator = new ParallelPrimeGenerator();
-        for (BigInteger prime :  parallelPrimeGenerator.getPrimes(3, 256)){
-            System.out.println("PrimeQ[" + prime.abs() + "]\n");
-        }
+        parallelPrimeGenerator.getPrimes(3, 256).forEach(p -> {
+            System.out.println("PrimeQ[" + p.abs() + "]\n");
+        });
     }
 }
